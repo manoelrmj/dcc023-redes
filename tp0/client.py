620656826490485959589c701d67f4956c3bec2d
Manoel <manoelrmiranda@gmail.com>
2018-03-25 00:28:08 -0300

    é tetra

--- tp0/client.py
+++ tp0/client.py
@@ -1,16 +1,43 @@
 import socket
-HOST = '127.0.0.1'
-# Endereco IP do Servidor
-PORT = 5000
-# Porta que o Servidor esta
-tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-dest = (HOST, PORT)
-tcp.connect(dest)
-print ('Para sair use CTRL+X\n')
-msg = input()
-while msg != '\x18':
-    tcp.send (msg.encode())
-    msg = tcp.recv(1024)
+import sys
+import struct
+
+def caesarCipher(string, shift):
+    return ''.join(map(lambda x:chr(((ord(x)+shift-97)%26)+97),string))
+
+def main():
+    # Endereco IP do Servidor
+    HOST = sys.argv[1]
+    # Porta
+    PORT = sys.argv[2]
+    # String a ser enviada
+    STRING_MSG = sys.argv[3]
+    # Tamanho da string
+    CIPHER = int(sys.argv[4])
+
+    # Cria o socket TCP
+    tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp.settimeout(15.0)
+    dest = (HOST, int(PORT))
+    tcp.connect(dest)
+
+    # Envia o tamanho da string
+    packed_int = struct.pack('>I', int(len(STRING_MSG.encode('utf-8'))))
+    tcp.send(packed_int)
+
+    # Envia a string
+    tcp.send(caesarCipher(STRING_MSG, CIPHER).encode())
+
+    # Envia a cifra
+    packed_int = struct.pack('>I', CIPHER)
+    tcp.send(packed_int)
+
+    # Recebe mensagem decifrada
+    msg = tcp.recv(len(STRING_MSG.encode('utf-8')))
     print (msg.decode())
-    msg = input()
-tcp.close()
+
+    # Fecha a conexão
+    tcp.close()
+
+if __name__ == '__main__':
+    main()
--- tp0/server.py
+++ tp0/server.py
@@ -1,19 +1,52 @@
 import socket
-HOST = '127.0.0.1'
-# Endereco IP do Servidor
-PORT = 5000
-# Porta que o Servidor esta
-tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-orig = (HOST, PORT)
-tcp.bind(orig)
-tcp.listen(1)
-con, cliente = tcp.accept()
-print ('Conectado por', cliente)
-while True:
-    msg = con.recv(1024).decode()
-    if not msg: break
-    msg = '>' + msg + '<'
-    print (cliente, msg)
-    con.send(msg.encode())
-print ('Finalizando conexao do cliente', cliente)
-con.close()
+import sys
+import _thread
+
+def decipher(string, cipher):
+    return ''.join(map(lambda x:chr(((ord(x)-cipher-97)%26)+97),string))
+
+def handleRequest(con):
+    con.settimeout(15.0)
+    # Recebe inteiro de 4 bytes (tamanho da string)
+    msg = con.recv(4)
+    string_size = int.from_bytes(msg, byteorder='big')
+
+    # Recebe a string
+    msg = con.recv(string_size)
+    string = msg.decode()
+
+    # Recebe a chave
+    msg = con.recv(4)
+    key = int.from_bytes(msg, byteorder='big')
+
+    # Decifra
+    decipheredString = decipher(string, key)
+    print(decipheredString)
+
+    # Envia mensagem decifrada
+    con.send (decipheredString.encode())
+
+    # Finaliza a conexao
+    con.close()
+
+def main():
+    # Endereco IP do Servidor
+    HOST = ''
+    # Porta
+    PORT = int(sys.argv[1])
+
+    # Cria o socket
+    tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    orig = (HOST, PORT)
+    tcp.bind(orig)
+    tcp.listen(10)
+
+    while True:
+        con, client = tcp.accept()
+        try:
+           _thread.start_new_thread(handleRequest, (con, ) )
+        except:
+           print ("Erro ao criar thread")
+
+if __name__ == '__main__':
+    main()
