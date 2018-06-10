6d6da5ddcb17fd18f640cb8fcdad17ad17a6a19e
Manoel <manoelrmiranda@gmail.com>
2018-06-10 15:47:44 -0300

    Load balancing

--- tp2/router.py
+++ tp2/router.py
@@ -56,7 +56,11 @@ class Router(object):
 
     def addLink(self, destinationAddr, weight):
         self.links[destinationAddr] = weight
-        self.routingTable[destinationAddr] = (int(weight), destinationAddr)
+        self.routingTable[destinationAddr] = {}
+        self.routingTable[destinationAddr]['weight'] = int(weight)
+        self.routingTable[destinationAddr]['hops'] = []
+        self.routingTable[destinationAddr]['hops'].append(destinationAddr)
+        self.routingTable[destinationAddr]['nextHop'] = 0
 
     def removeLink(self, destinationAddr):
         del self.links[destinationAddr]
@@ -87,8 +91,7 @@ class Router(object):
         UDP_MESSAGE['destination'] = destination
         UDP_MESSAGE['hops'] = []
         UDP_MESSAGE['hops'].append(self.addr)
-        nextHop = self.routingTable[destination][1]
-        self.sock.sendto(json.dumps(UDP_MESSAGE).encode(), (nextHop, self.UDP_PORT))
+        self.sock.sendto(json.dumps(UDP_MESSAGE).encode(), (self._nextHop(destination), self.UDP_PORT))
 
     def _cliThread(self):
         # Command Line Interface
@@ -110,6 +113,19 @@ class Router(object):
 
         return
 
+    def _nextHop(self, destination):
+        nextHops = self.routingTable[destination]['hops']
+        if(len(nextHops) > 1): # Load balancing
+            nextHop = nextHops[self.routingTable[destination]['nextHop']]
+            self.routingTable[destination]['nextHop'] += 1
+            if(self.routingTable[destination]['nextHop'] > len(nextHops)-1):
+                self.routingTable[destination]['nextHop'] = 0 # Volta para a primeira rota
+            return nextHop
+
+        else:
+            return nextHops[0]
+
+
     def _recThread(self):
         #self.sock = socket.socket(socket.AF_INET, # Internet
         #             socket.SOCK_DGRAM, 0) # UDP
@@ -120,29 +136,30 @@ class Router(object):
             json_data = json.loads(data.decode())
             #print(json_data['type'])
             if(json_data['type'] == 'update'):
-                #print(json_data)
-                print("Update from ", source_addr)
-                # for key,value in json_data['distances'].items():
-                #     print (key,value)
+                #print("Update from ", source_addr)
                 # Atualizar tabela de roteamento
                 for key,value in json_data['distances'].items():
                     if(key in self.routingTable):
-                        if(value + int(self.links[source_addr[0]]) < self.routingTable[key][0]):
-                            self.routingTable[key] = (int(value) + int(self.links[source_addr[0]]), source_addr[0])
-                        elif(value + int(self.links[source_addr[0]]) == self.routingTable[key][0]):
-                            if(source_addr[0] not in self.routingTable[key]):
-                                self.routingTable[key] += (source_addr[0],)
+                        if(value + int(self.links[source_addr[0]]) < self.routingTable[key]['weight']):
+                            self.routingTable[key]['weight'] = int(value) + int(self.links[source_addr[0]])
+                            self.routingTable[key]['hops'] = []
+                            self.routingTable[key]['hops'].append(source_addr[0])
+                        elif(value + int(self.links[source_addr[0]]) == self.routingTable[key]['weight']):
+                            if(source_addr[0] not in self.routingTable[key]['hops']):
+                                self.routingTable[key]['hops'].append(source_addr[0])
                     else: 
-                        self.routingTable[key] = (int(value) + int(self.links[source_addr[0]]), source_addr[0])
-                print("routingTable:")
-                print(self.routingTable)
+                        self.routingTable[key] = {}
+                        self.routingTable[key]['weight'] = int(value) + int(self.links[source_addr[0]])
+                        self.routingTable[key]['hops'] = []
+                        self.routingTable[key]['hops'].append(source_addr[0])
+                #print("routingTable:")
+                #print(self.routingTable)
             elif(json_data['type'] == 'data'):
                 if(json_data['destination'] == self.addr): # Pacote chegou ao destino
                     print("# Mensagem recebida por ", json_data['source'])
                     print(json_data['payload'])
                 else: # Encaminha
-                    ## TODO: balanceamento de carga ##
-                    nextHop = self.routingTable[json_data['destination']][1]
+                    nextHop = self._nextHop(json_data['destination'])
                     print("Fowrading message from ", source_addr[0], " to ", nextHop)
                     self.sock.sendto(json.dumps(json_data).encode(), (nextHop, self.UDP_PORT))
             elif(json_data['type'] == 'trace'):
@@ -155,10 +172,10 @@ class Router(object):
                     UDP_MESSAGE['source'] = self.addr
                     UDP_MESSAGE['destination'] = json_data['source']
                     UDP_MESSAGE['payload'] = json_data
-                    nextHop = self.routingTable[json_data['source']][1]
+                    nextHop = self._nextHop(json_data['source'])
                     self.sock.sendto(json.dumps(UDP_MESSAGE).encode(), (nextHop, self.UDP_PORT))
                 else: # Ecaminha trace
-                    nextHop = self.routingTable[json_data['destination']][1]
+                    nextHop = self._nextHop(json_data['destination'])
                     print("Fowrading trace to ", json_data['destination'])
                     json_data['hops'].append(self.addr)
                     self.sock.sendto(json.dumps(json_data).encode(), (nextHop, self.UDP_PORT))
@@ -184,7 +201,7 @@ class Router(object):
                 UDP_MESSAGE['destination'] = neighbour
                 for key, value in self.routingTable.items():
                     if(key != neighbour): # Split horizon
-                        UDP_MESSAGE['distances'][key] = value[0]
+                        UDP_MESSAGE['distances'][key] = value['weight']
             
                 self.sock.sendto(json.dumps(UDP_MESSAGE).encode(), (neighbour, self.UDP_PORT))
 
